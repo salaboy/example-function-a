93e1f6bdcf18d391ac2ddd053d1547eff5f4bd60
salaboy <salaboy@gmail.com>
2019-05-25 09:37:06 +0200

    minor clean up

--- src/main/java/org/salaboy/k8s/knative/fn/FunctionController.java
+++ src/main/java/org/salaboy/k8s/knative/fn/FunctionController.java
@@ -2,10 +2,7 @@ package org.salaboy.k8s.knative.fn;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
-import java.text.SimpleDateFormat;
 
 @RestController
 public class FunctionController {
@@ -20,18 +17,19 @@ public class FunctionController {
     @GetMapping("/")
     public String calculateDiscount() {
         count++;
-        return String.format(RESPONSE_STRING_FORMAT, prefix, "", HOSTNAME, count);
+        return "Function A call: " + count;
     }
 
     @PostMapping("/")
-    public @ResponseBody String eventCalculate(@RequestBody String cloudEventJson) {
+    public @ResponseBody
+    String eventCalculate(@RequestBody String cloudEventJson) {
         count++;
         //  String greeterHost = String.format(RESPONSE_STRING_FORMAT, ""," Event ", HOSTNAME, count);
 
         //   cloudEventJson
 //                                  .put("host",greeterHost.replace("\n","").trim())
 //                                  .put("time",SDF.format(new Date()));
-        LOGGER.info("Event Message Received \n {}",cloudEventJson);
+        LOGGER.info("Event Message Received \n {}", cloudEventJson);
         return "Event Message Received: " + cloudEventJson;
     }
 
